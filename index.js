31b722b3215074a1a7a90520273884de5f9f8568
Andrea Brezanova <55324009+Andrea-Brezanova@users.noreply.github.com>
2025-03-14 22:06:52 +0100

    Added more GET requests.

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// make an HTTP request to the root of the server
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+}
+);
+
+// make an HTTP request to the /about route
+app.get("/about", (req, res) => {
+  res.send("<h1>About Page</h1><p>This is the about page.</p>");
+}
+);
+
+// make an HTTP request to the /contact route
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Page</h1><p>This is the contact page.</p>");
+}
+);
+
+// start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
